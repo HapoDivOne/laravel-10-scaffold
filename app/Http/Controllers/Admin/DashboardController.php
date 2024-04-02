48dc18657671bf7b42f2e7d1bd6f24ce8ea7fdd3
hapo-congbv <cong.bv@haposoft.com>
2024-04-02 13:42:18 +0700

    Update: file lang

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -10,6 +10,6 @@ class DashboardController extends Controller
 {
     public function index(): View
     {
-        return View('admin.dashboard');
+        return view('admin.dashboard');
     }
 }
