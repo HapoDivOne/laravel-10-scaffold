215158afaa4097d3b6b2a0dd385db236ae357127
hapo-congbv <cong.bv@haposoft.com>
2024-04-02 13:50:33 +0700

    Update: added comment function index dashboard admin

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,10 +4,13 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
+    /**
+     * show screen admin dashboard
+     *
+     */
     public function index(): View
     {
         return view('admin.dashboard');
